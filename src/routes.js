ac24340c84cdf0fbe77159a9f132f738835103f9
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-02-17 13:07:50 -0300

    feat: configuração react navigation

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+// qualquer tipo de navegação deve ser colocado aqui...
+const Routes = createAppContainer(
+   createStackNavigator(
+      {
+         Main,
+         User,
+      },
+      {
+         defaultNavigationOptions: {
+            headerStyle: {
+               backgroundColor: '#7159c1',
+            },
+            headerTintColor: '#FFF',
+            headerTitleAlign: 'center',
+            headerBackTitleVisible: false,
+         },
+      }
+   )
+);
+
+export default Routes;
