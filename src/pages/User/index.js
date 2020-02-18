3e68a3c7f89e8d7403f5860b64e9aecdbe2b654c
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-02-17 21:50:23 -0300

    feat: listando os favoritos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
 
 import api from '../../services/api';
-// import { Container } from './styles';
+
+import {
+   Container,
+   Header,
+   Avatar,
+   Name,
+   Bio,
+   Stars,
+   Starred,
+   OwnerAvatar,
+   Info,
+   Title,
+   Author,
+} from './styles';
 
 export default class User extends Component {
    static navigationOptions = ({ navigation }) => ({
@@ -31,8 +43,35 @@ export default class User extends Component {
    }
 
    render() {
+      const { navigation } = this.props;
       const { stars } = this.state;
+      const user = navigation.getParam('user');
 
-      return <View />;
+      return (
+         <Container>
+            <Header>
+               <Avatar source={{ uri: user.avatar }} />
+               <Name>{user.name}</Name>
+               <Bio>{user.bio}</Bio>
+            </Header>
+
+            <Stars
+               data={stars}
+               keyExtractor={star => String(star.id)}
+               onEndReached={() => {
+                  console.log(`chegou no fim da lista`);
+               }}
+               renderItem={({ item }) => (
+                  <Starred>
+                     <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                     <Info>
+                        <Title>{item.name}</Title>
+                        <Author>{item.owner.login}</Author>
+                     </Info>
+                  </Starred>
+               )}
+            />
+         </Container>
+      );
    }
 }
